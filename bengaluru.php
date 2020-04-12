2b5780f514ba0c8a7a41fc07f3c6f84bfa5a992f
callcoder <40206877+callcoder@users.noreply.github.com>
2020-04-12 22:56:25 +0530

    page for bengaluru branch

---
+++ bengaluru.php
@@ -0,0 +1,82 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="hicon.jpg">
+<title>Bengaluru</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script>
+  function appt()
+  {
+    alert("To book an appointment, call 9903918822");
+  }
+  </script>
+  <style>
+  .button {
+  background-color: black;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  }
+  </style>
+</head>
+<body>
+  <div class="container">
+
+<br><br>
+    <div class="card" style="width:400px">
+      <center>
+      <img class="card-img-top" src="blrdr1.png" alt="Card image" style="width:90%">
+    </center>
+      <div class="card-body">
+        <h4 class="card-title">Dr Ashutosh</h4>
+        <p class="card-text">
+              DIRECTOR<br>
+              VASCULAR SURGERY<br>
+              SANJIVANI HOSPITAL,BENGALURU
+        </p>
+        <center><a href="#" class="btn btn-primary stretched-link" onClick="return appt(); return false;">Book Appointment</a></center>
+      </div>
+</div>
+
+      <br><br><br>
+      <div class="card" style="width:400px">
+        <center><img class="card-img-top" src="blrdr2.jpg" alt="Card image" style="width:90%"></center>
+        <div class="card-body">
+          <h4 class="card-title">Dr Ridhima</h4>
+          <p class="card-text">
+            DIRECTOR<br>
+            PAEDIATRICS<br>
+            SANJIVANI HOSPITAL,BENGALURU
+          </p>
+          <center><a href="#" class="btn btn-primary stretched-link" onClick="return appt(); return false;">Book Appointment</a></center>
+        </div>
+  </div>
+
+  <br><br><br>```1`
+  <div class="card" style="width:400px">
+    <center><img class="card-img-top" src="blrdr3.jpg" alt="Card image" style="width:90%"></center>
+    <div class="card-body">
+      <h4 class="card-title">Dr Anjali</h4>
+      <p class="card-text">
+        DIRECTOR<br>
+        NEUROLOGY<br>
+        SANJIVANI HOSPITAL,BENGALURU
+      </p>
+      <center><a href="#" class="btn btn-primary stretched-link" onClick="return appt(); return false;">Book Appointment</a></center>
+    </div>
+</div>
+<button type="button" class="button" id="bt2">
+<a href="appt1.php"><b><i>Go Back</i></b></a>
+</body>
+</html>
